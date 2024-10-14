b08c7afa64821761e4b58cece0bf87fda1ff0452
MuhammadHamzaWaqar1317 <waqarhamza2003@gmail.com>
2024-10-15 00:21:40 +0500

    feat : added Signup/SignIn Thunks, made UserSlice to manage state

--- src/Components/Authenticate/signup.jsx
+++ src/Components/Authenticate/signup.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { Form, Input, Button } from "antd";
 // useNavigate
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signUpThunk } from "../../Redux/Thunks/UserApi";
 // Login CSS
 import "./login.scss";
 // Img
-import login from "../../assets/signup.png";
+// import login from "../../assets/signup.png";
 
 function Signup() {
   // useNavigate Variable
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const socketId = useSelector((state) => state.userSlice.socketId);
   // Password Logic
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const handleMouseDown = () => {
-    setIsPasswordVisible(true);
-  };
-  const handleMouseUp = () => {
-    setIsPasswordVisible(false);
+
+  const handleFinish = (body) => {
+    dispatch(signUpThunk({ ...body, socketId }));
   };
 
   // Main Body
@@ -26,45 +28,33 @@ function Signup() {
         <div className="Sub_Login_Whole">
           <div className="Login_Main">
             <div className="Login_Main_Part_2">
-              <img src={login} alt="NA" />
+              {/* <img src={login} alt="NA" /> */}
               <p>Join us by creating an account with your email</p>
             </div>
             <div className="Login_Main_Part_1">
               <h1>Sign Up</h1>
               <div className="Login_Main_Part_Sub">
                 <p className="Login_P">Create Your New Account</p>
-                {/* Field Box */}
-                <div className="Login_Field_Box">
-                  <input type="text" placeholder="Enter Your Name " />
-                </div>
-                {/* Field Box */}
-                <div className="Login_Field_Box">
-                  <input type="text" placeholder="Enter Your Email " />
-                </div>
-                {/* Field Box */}
-                <div className="Login_Field_Box">
-                  <input
-                    type={isPasswordVisible ? "text" : "password"}
-                    placeholder="Enter Your Password"
-                  />
-                  {/* Eye icon for toggling visibility */}
-                  <div
-                    className="My_Pass_Box"
-                    onMouseDown={handleMouseDown}
-                    onMouseUp={handleMouseUp}
-                    onMouseLeave={handleMouseUp}
-                  >
-                    <i
-                      className={
-                        isPasswordVisible ? "fa fa-eye-slash" : "fa fa-eye"
-                      }
-                    />
-                  </div>
-                </div>
-                {/* Field Box */}
-                <div className="Login_Field_Box">
-                  <button>Sign Up</button>
-                </div>
+
+                <Form onFinish={handleFinish}>
+                  <Form.Item name={"name"}>
+                    <Input placeholder="Enter Name"></Input>
+                  </Form.Item>
+                  <Form.Item name={"email"}>
+                    <Input type="email" placeholder="Enter Email"></Input>
+                  </Form.Item>
+                  <Form.Item name={"password"}>
+                    <Input.Password
+                      type="email"
+                      placeholder="Enter Password"
+                    ></Input.Password>
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button htmlType="submit">SignUp</Button>
+                  </Form.Item>
+                </Form>
+
                 {/* Field Box Last */}
                 <div className="Login_Field_Box_Last">
                   <div className="Login_Last_Part_1">
