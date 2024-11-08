4feabfbe017fba4e82a6a404f485fc16946aae1e
MuhammadHamzaWaqar1317 <waqarhamza2003@gmail.com>
2024-11-09 00:21:15 +0500

    feat :  added URI obj for API routes

--- src/Api/Endpoints/ApiEndpoints.js
+++ src/Api/Endpoints/ApiEndpoints.js
@@ -4,72 +4,78 @@ const authEndpoint = "authenticated";
 
 const unauthEndpoint = "unauthenticated";
 
-export const signUp = (body) => api.post(`${unauthEndpoint}/sign-up`, body);
+const URI = {
+  signUp: `${unauthEndpoint}/sign-up`,
+  signIn: `${unauthEndpoint}/sign-in`,
+  forgetPassword: `${unauthEndpoint}/forget-password`,
+  verifyOTP: `${unauthEndpoint}/verify-otp`,
+  createNewPassword: `${authEndpoint}/employee/create-new-password`,
+  userInfo: `${authEndpoint}/employee`,
+  publicMenu: `${unauthEndpoint}/menu`,
+  privateMenu: `${authEndpoint}/admin/menu`,
+  publicMenuCategory: `${unauthEndpoint}/menu-category`,
+  privateMenuCategory: `${authEndpoint}/admin/menu-category`,
+  privateBranch: `${authEndpoint}/admin/branch`,
+  privateReservation: `${authEndpoint}/admin/reservation`,
+  privateOrder: `${authEndpoint}/admin/order`,
+};
 
-export const signIn = (body) => api.post(`${unauthEndpoint}/sign-in`, body);
+export const signUp = (body) => api.post(URI.signUp, body);
 
-export const forgetPassword = (body) =>
-  api.post(`${unauthEndpoint}/forget-password`, body);
+export const signIn = (body) => api.post(URI.signIn, body);
 
-export const verifyOtp = (body) =>
-  api.post(`${unauthEndpoint}/verify-otp`, body);
+export const forgetPassword = (body) => api.post(URI.forgetPassword, body);
+
+export const verifyOtp = (body) => api.post(URI.verifyOTP, body);
 
 export const createNewPassword = (body) =>
-  api.post(`${authEndpoint}/employee/create-new-password`, body);
+  api.post(URI.createNewPassword, body);
 
-export const getUserInfo = () => api.get(`${authEndpoint}/employee`);
+export const getUserInfo = () => api.get(URI.userInfo);
 
-export const getMenu = () => api.get(`${unauthEndpoint}/menu`);
+export const getMenu = () => api.get(URI.publicMenu);
 
-export const addMenu = (menuItem) =>
-  api.post(`${authEndpoint}/admin/menu`, menuItem);
+export const addMenu = (menuItem) => api.post(URI.privateMenu, menuItem);
 
-export const updateMenu = (menuItem) =>
-  api.patch(`${authEndpoint}/admin/menu`, menuItem);
+export const updateMenu = (menuItem) => api.patch(URI.privateMenu, menuItem);
 
 export const deleteMenu = (menuItemId) =>
-  api.delete(`${authEndpoint}/admin/menu?${menuItemId.toString()}`);
+  api.delete(`${URI.privateMenu}?${menuItemId.toString()}`);
 
-export const getMenuCategory = () => api.get(`${unauthEndpoint}/menu-category`);
+export const getMenuCategory = () => api.get(URI.publicMenuCategory);
 
 export const addMenuCategory = (MenuCategory) =>
-  api.post(`${authEndpoint}/admin/menu-category`, MenuCategory);
+  api.post(URI.privateMenuCategory, MenuCategory);
 
 export const updateMenuCategory = (MenuCategory) =>
-  api.patch(`${authEndpoint}/admin/menu-category`, MenuCategory);
+  api.patch(URI.privateMenuCategory, MenuCategory);
 
 export const deleteMenuCategory = (menuCategoryId) =>
-  api.delete(
-    `${authEndpoint}/admin/menu-category?${menuCategoryId.toString()}`
-  );
+  api.delete(`${URI.privateMenuCategory}?${menuCategoryId.toString()}`);
 
-export const getBranch = () => api.get(`${authEndpoint}/admin/branch`);
+export const getBranch = () => api.get(URI.privateBranch);
 
-export const addBranch = (branch) =>
-  api.post(`${authEndpoint}/admin/branch`, branch);
+export const addBranch = (branch) => api.post(URI.privateBranch, branch);
 
-export const updateBranch = (branch) =>
-  api.patch(`${authEndpoint}/admin/branch`, branch);
+export const updateBranch = (branch) => api.patch(URI.privateBranch, branch);
 
 export const deleteBranch = (branchId) =>
-  api.delete(`${authEndpoint}/admin/branch?${branchId.toString()}`);
+  api.delete(`${URI.privateBranch}?${branchId.toString()}`);
 
-export const getReservation = () =>
-  api.get(`${authEndpoint}/admin/reservation`);
+export const getReservation = () => api.get(URI.privateReservation);
 
 export const addReservation = (reservation) =>
-  api.post(`${authEndpoint}/admin/reservation`, reservation);
+  api.post(URI.privateReservation, reservation);
 
 export const updateReservation = (reservation) =>
-  api.patch(`${authEndpoint}/admin/reservation`, reservation);
+  api.patch(URI.privateReservation, reservation);
 
 export const deleteReservation = (reservationId) =>
-  api.delete(`${authEndpoint}/admin/reservation?${reservationId.toString()}`);
+  api.delete(`${URI.privateReservation}?${reservationId.toString()}`);
 
-export const addOrder = (order) =>
-  api.post(`${authEndpoint}/admin/order`, order);
+export const addOrder = (order) => api.post(URI.privateOrder, order);
 
-export const getOrder = () => api.get(`${authEndpoint}/admin/order`);
+export const getOrder = () => api.get(URI.privateOrder);
 
 export const updateOrderStatus = (orderStatus) =>
-  api.patch(`${authEndpoint}/admin/order`, orderStatus);
+  api.patch(URI.privateOrder, orderStatus);
